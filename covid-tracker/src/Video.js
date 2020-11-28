559a69f80581b47102f1f59b33cc07071717f02c
M_813931 <m_813931@ambus130625.myfiosgateway.com>
2020-11-27 19:23:47 -0500

    first commit

---
+++ covid-tracker/src/Video.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Video.css"
+function video() {
+    return (
+        <div>
+        <iframe src="https://www.youtube.com/embed/DCdxsnRF1Fk?autoplay=1" title="info corona" /><br/>
+        <p>
+            <div>
+
+            </div>
+        </p>
+        </div>
+    )
+}
+
+export default video
---
+++ covid-tracker/src/about.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="About">
+            <p>
+            Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.<br/>
+Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.  Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.<br/>
+The best way to prevent and slow down transmission is to be well informed about the COVID-19 virus, the disease it causes and how it spreads. Protect yourself and others from infection by washing your hands or using an alcohol based rub frequently and not touching your face.<br/> 
+The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes, so it’s important that you also practice respiratory etiquette (for example, by coughing into a flexed elbow).
+            </p>
+            <p>
+            COVID-19 affects different people in different ways. Most infected people will develop mild to moderate illness and recover without hospitalization.<br/>
+Most common symptoms:<br/>
+            <ul>
+            <li>fever</li> 
+            <li>dry cough</li>
+            <li>tiredness</li>
+            </ul>
+            </p>
+            <p>
+            Less common symptoms:<br/>
+            <ul>
+            <li>fever</li> 
+            <li>dry cough</li>
+            <li>tiredness</li>
+            </ul>
+            </p>
+        </div>
+    )
+}
